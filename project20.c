86219c56ab6865726b321d94b00b536f2996028c
jemee2211 <120568492+jemee2211@users.noreply.github.com>
2022-12-26 17:42:27 +0530

    Add files via upload

---
+++ project20.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main(){
+	
+	
+	int r,c;
+	int a = 1;
+	
+	for(r=1; r<=5; r++){
+		for(c=1; c<=r; c++){	
+		
+		
+		
+			
+			printf("%d",a);
+			
+			
+			a++;
+		}
+			printf("\n");
+	}
+
+}
